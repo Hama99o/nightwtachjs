6037273aef9757b851e3224967832ea58b33a965
Hama <hmms@gmail.com>
2021-04-21 12:03:33 +0200

    new update

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,8 +1,8 @@
 module.exports = {
   'My first test case'(browser){
     browser
-      .url('http://localhost:8080/')
-      .waitForElementVisible('.articles')
-      .assert.containsText(".articles", "Articles")
+      .url('https://github.com/Hama99o/')
+      .waitForElementVisible('.vcard-fullname')
+      .assert.containsText(".vcard-fullname", "Hama")
   }
 }
